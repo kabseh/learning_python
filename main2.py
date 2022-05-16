ab19800a20b5ed35cf3ab4b9defbe7f011660f0f
DESKTOP-U91OOBK\Abod <a.abod.k2002.gmail.com>
2022-05-16 22:30:53 +0300

    print my name

---
+++ main2.py
@@ -0,0 +1,10 @@
+myVariable = "AbodK"
+print(myVariable)
+
+print("abodk\nlove\nmlukheah\nand\nlove\nrice")
+
+# carriage ruturn
+print("12345678910121314151617181920\rabodk love girls")
+
+
+print("\x41\x62\x64\x75\x6C\x72\x61\x68\x6D\x61\x6E")

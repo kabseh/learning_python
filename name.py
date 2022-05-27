9e345e17d2f56f08e28780042ac3ccfaf1fde787
DESKTOP-U91OOBK\Abod <a.abod.k2002.gmail.com>
2022-05-27 18:36:42 +0300

    now you can sign in

--- name.py
+++ name.py
@@ -1,9 +1,15 @@
+print("=" * 50)
+
 fName = input("whats your first name?")
 mName = input("whats your middle name?")
 lName = input("whats your last name?")
+email = input("what\'s your Email")
+website = email[email.index("@")+1:]
 
 fName = fName.capitalize().strip()
 mName = mName.capitalize().strip()
 lName = lName.capitalize().strip()
+email = email.strip()
 
 print(f"Hello {fName} {mName} {lName} Nice to meet you")
+print("your Email is " f"{email}\nYour Websit is {website}\n Wellcome Sir")

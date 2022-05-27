18b278a32fcb5514813048b665851c62bcf0a165
DESKTOP-U91OOBK\Abod <a.abod.k2002.gmail.com>
2022-05-27 15:13:36 +0300

    Say Hello

---
+++ name.py
@@ -0,0 +1,9 @@
+fName = input("whats your first name?")
+mName = input("whats your middle name?")
+lName = input("whats your last name?")
+
+fName = fName.capitalize().strip()
+mName = mName.capitalize().strip()
+lName = lName.capitalize().strip()
+
+print(f"Hello {fName} {mName} {lName} Nice to meet you")
